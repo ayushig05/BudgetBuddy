f0e3efdbbccea9d9a6e9b2fa6acd0ba121a828ee
Ayushi Gupta <118197038+ayushig05@users.noreply.github.com>
2024-07-24 15:35:23 +0530

    files added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import Navbar from "./components/navbar";
+import Info from "./components/info";
+import Stats from "./components/stats";
+import Charts from "./components/charts";
+import DoughnoutCharts from "./components/doughnoutCharts";
+import Transactions from "./components/transactions";
+import Accounts from "./components/accounts";
+
 const App = () => {
-  return (
-    <div className="h-48 text-3xl underline">
-      Expense Tracker
+  const theme = "light";
+  return <main className= {theme}>
+    <div className="w-full px-6 md:px-20 bg-white dark:bg-slate-900">
+      <Navbar />
+      <div className="px-0 md:px-5 2xl:px-20">
+        <Info />
+        <Stats />
+        <div className="w-full flex flex-col-reverse md:flex-row items-center gap-10">
+          <Charts />
+          <DoughnoutCharts />
+        </div>
+        <div className="flex flex-col-reverse md:flex-row gap-0 md:gap-10 2xl:gap-20">
+          <Transactions />
+          <Accounts />
+        </div>
+      </div>
     </div>
-  );
-}
+  </main>
+};
 
 export default App;
---
+++ src/components/doughnoutCharts.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DoughnoutCharts = () => {
+  return (
+    <div>DoughnoutCharts</div>
+  )
+}
+
+export default DoughnoutCharts;
