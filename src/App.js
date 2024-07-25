cf45f4aa62bb572d8b98ccf77826982deace4247
Ayushi Gupta <118197038+ayushig05@users.noreply.github.com>
2024-07-25 21:39:39 +0530

    updated navbar

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import Charts from "./components/charts";
 import DoughnoutCharts from "./components/doughnoutCharts";
 import Transactions from "./components/transactions";
 import Accounts from "./components/accounts";
+import { useStore } from './store/useStore';
 
 const App = () => {
-  const theme = "light";
+  const theme = useStore((state) => state.theme);
   return <main className= {theme}>
     <div className="w-full px-6 md:px-20 bg-white dark:bg-slate-900">
       <Navbar />
       <div className="px-0 md:px-5 2xl:px-20">
-        <Info />
+        <Info title='Dashboard' subTitle='Monitor your financial activities'/>
         <Stats />
         <div className="w-full flex flex-col-reverse md:flex-row items-center gap-10">
           <Charts />
---
+++ src/store/useStore.js
@@ -0,0 +1,6 @@
+import { create } from 'zustand';
+
+export const useStore = create((set) => ({
+  theme: localStorage.getItem("theme") ?? "light",
+  setTheme: (value) => set({ theme: value }),
+}));
