cc3c8abdd2f1516a80df36511d6c079222b55d5c
Ayushi Gupta <118197038+ayushig05@users.noreply.github.com>
2024-07-26 13:28:50 +0530

    updated

---
+++ src/firebase-config/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCs68js7-vsMCAwf3rd4ScE0CxOjj8-BK4",
+  authDomain: "expense-tracker-5d68d.firebaseapp.com",
+  projectId: "expense-tracker-5d68d",
+  storageBucket: "expense-tracker-5d68d.appspot.com",
+  messagingSenderId: "3791524007",
+  appId: "1:3791524007:web:0c19ee580321a06bd00b82"
+};
+
+const app = initializeApp(firebaseConfig);
