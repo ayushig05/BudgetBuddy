fb7edd670adbee551a98d12ab49c17425c6b71a0
Ayushi Gupta <118197038+ayushig05@users.noreply.github.com>
2024-08-30 13:09:23 +0530

    final commit

--- src/components/pages/tracker.js
+++ src/components/pages/tracker.js
@@ -27,17 +27,17 @@ const Tracker = () => {
     {
       label: "Your Total Balance",
       amount: balance >= 0 ? `$${balance}` : `-$${balance * -1}`,
-      icon: <BsCurrencyDollar size={26} />,
+      icon: <BsCurrencyDollar size={20} />,
     },
     {
       label: "Total Income",
       amount: `$${income}`,
-      icon: <BsCashCoin size={26} />,
+      icon: <BsCashCoin size={20} />,
     },
     {
       label: "Total Expense",
       amount: `$${expenses}`,
-      icon: <SiCashapp size={26} />,
+      icon: <SiCashapp size={20} />,
     },
   ];
 
@@ -48,9 +48,8 @@ const Tracker = () => {
   ];
 
   useEffect(() => {
-    const authData = JSON.parse(localStorage.getItem("auth"));
-    console.log("Local Storage Auth Data:", authData);
-  }, []);
+    console.log("Fetched Transactions:", transactions);
+  }, [transactions]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -75,14 +74,14 @@ const Tracker = () => {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-800 py-10">
-      <div className="w-full max-w-6xl min-h-full bg-white p-8 rounded-lg shadow-md">
+    <div className="flex justify-center items-center min-h-screen bg-gray-800">
+      <div className="w-full max-w-6xl min-h-full bg-white p-4 rounded-lg shadow-md">
         <div className="flex justify-between items-center mb-6">
           <div className="flex items-center gap-2 cursor-pointer">
-            <div className="w-10 h-10 md:w-12 md:h-12 flex items-center justify-center bg-violet-700 rounded-xl">
-              <RiCurrencyLine className="text-white text-3xl w-20 hover:animate-spin" />
+            <div className="w-8 h-8 md:w-10 md:h-10 flex items-center justify-center bg-violet-700 rounded-xl">
+              <RiCurrencyLine className="text-white text-xl w-14 hover:animate-spin" />
             </div>
-            <h1 className="text-4xl font-bold text-black">Expense Tracker</h1>
+            <h1 className="text-3xl font-bold text-black">Expense Tracker</h1>
           </div>
           {profilePhoto && (
             <div className="flex justify-center gap-2">
@@ -92,12 +91,12 @@ const Tracker = () => {
                 className="w-10 md:w-12 h-10 md:h-12 rounded-full object-cover cursor-pointer"
               />
               <div className="block cursor-pointer">
-                <p className="text-lg font-medium text-black">{name}</p>
-                <span className="text-sm text-gray-700">{email}</span>
+                <p className="text-base font-medium text-black">{name}</p>
+                <span className="text-xs text-gray-700">{email}</span>
               </div>
               <button
                 onClick={handleSignOut}
-                className="mt-4 bg-red-500 text-white px-4 py-2 rounded"
+                className="mt-3 bg-red-500 text-white px-2 py-1 rounded"
               >
                 Sign Out
               </button>
@@ -108,19 +107,19 @@ const Tracker = () => {
           {data.map((item, index) => (
             <div
               key={index + item.label}
-              className="w-full 2xl:min-w-60 h-16 flex items-center justify-between gap-5 px-4 py-12 rounded-lg bg-gray-100 border border-gray-100 0"
+              className="w-72 2xl:min-w-60 h-16 flex items-center justify-between gap-5 px-4 py-8 rounded-lg bg-gray-100 border border-gray-100 0"
             >
               <div className="flex items-center gap-2">
                 <div
-                  className={`w-12 h-12 flex items-center justify-center rounded-full ${ICON_STYLES[index]}`}
+                  className={`w-8 h-8 flex items-center justify-center rounded-full ${ICON_STYLES[index]}`}
                 >
                   {item.icon}
                 </div>
-                <div className="space-y-3">
-                  <span className="text-gray-600  text-base md:text-lg">
+                <div>
+                  <span className="text-gray-600 text-base md:text-base">
                     {item.label}
                   </span>
-                  <p className="text-2xl 2xl:text-3xl font-medium text-black ">
+                  <p className="text-base 2xl:text-2xl font-medium text-black ">
                     {item.amount}
                   </p>
                 </div>
@@ -150,7 +149,7 @@ const Tracker = () => {
               className="w-80 p-2 border rounded"
             />
           </div>
-          <div className="flex mb-4">
+          <div className="flex mb-1">
             <div className="mr-4">
               <input
                 type="radio"
@@ -160,7 +159,7 @@ const Tracker = () => {
                 onChange={(e) => setTransactionType(e.target.value)}
                 className="mr-1 cursor-pointer"
               />
-              <label htmlFor="expense" className="cursor-pointer">
+              <label htmlFor="expense" className="cursor-pointer text-sm">
                 Expense
               </label>
             </div>
@@ -171,23 +170,23 @@ const Tracker = () => {
                 value="income"
                 checked={transactionType === "income"}
                 onChange={(e) => setTransactionType(e.target.value)}
-                className="mr-1 cursor-pointer"
+                className="mr-1 cursor-pointer mt-1"
               />
-              <label htmlFor="income" className="cursor-pointer">
+              <label htmlFor="income" className="cursor-pointer text-sm">
                 Income
               </label>
             </div>
           </div>
           <button
-            className="bg-blue-400 text-white px-4 py-2 rounded"
+            className="bg-blue-400 text-white text-base mt-2 px-4 py-1 rounded"
             type="submit"
           >
             Add Transaction
           </button>
         </form>
-        <div className="mt-8">
+        <div>
           <h3 className="text-lg font-semibold text-center">Transactions</h3>
-          <ul className="mt-4">
+          <ul className="mt-2">
             {transactions.length > 0 ? (
               transactions.map((transaction) => {
                 const { description, transactionAmount, transactionType, id } =
